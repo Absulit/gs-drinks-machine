0f9e8a4c7c7c5a48d1c1a59cff302b20c23808ef
Sebastian Sanabria <sebastian@absulit.com>
2021-06-05 11:03:54 -0600

    Project Setup and Basic form
    - creating project
    - basic form in html view

---
+++ DrinksMachine/Models/Drink.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinksMachine.Models
+{
+    public abstract class Drink
+    {
+        public string Name;
+        public int QuantityAvailable;
+    }
+}
